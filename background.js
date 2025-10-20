489203277ceb027acac53845714c5b2b08ef6115
BeMounir <mounir.bekkaoui@student.hu.nl>
2025-10-20 13:48:41 +0200

    added background

---
+++ background.js
@@ -0,0 +1,6 @@
+const images = 'images/backgrounds/background.png';
+
+document.body.style.background = `
+    url('${images}') no-repeat center center fixed
+  `;
+document.body.style.backgroundSize = 'cover';
